86dc032a4c1a0b6157b152d0c858dcd119185e8e
Achal <achaldev3@gmail.com>
2023-08-08 14:58:04 +0530

    change

--- components/ImageContainer.jsx
+++ components/ImageContainer.jsx
@@ -1,5 +1,27 @@
+import bgm from "@/public/pattern-bg-mobile.png";
+import bgd from "@/public/pattern-bg-desktop.png";
 const ImageContainer = () => {
-  return <div className="image-container"></div>;
+  const width = screen.width;
+
+  if (width < 1200) {
+    return (
+      <div
+        className="image-container"
+        style={{
+          backgroundImage: `url(${bgm.src})`,
+        }}
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        className="image-container"
+        style={{
+          backgroundImage: `url(${bgd.src})`,
+        }}
+      ></div>
+    );
+  }
 };
 
 export default ImageContainer;
